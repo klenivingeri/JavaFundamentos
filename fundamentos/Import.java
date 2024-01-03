d201dcfa1902f0955429bd973f283209ec67bb35
klenivingeri <libertti@live.com>
2024-01-03 00:38:29 -0300

    chore: imports

---
+++ fundamentos/Import.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+import java.util.Date;
+
+public class Import {
+
+	public static void main(String[] args) {
+		Date d = new Date();
+		System.out.println(d);
+		
+	//	JButton botao = new JButton();
+		
+	}
+	// Ctrl + shift + o , ajuda a organizar os imports
+}
